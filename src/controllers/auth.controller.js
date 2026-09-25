b6b32220560766a00ca2c17d8017a9050ecc9f0e
Pranav Birla <pranavbirla06@gmail.com>
2026-09-26 02:21:27 +0530

    Solved the problem of role fetching in the auth controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -194,6 +194,7 @@ const getMe = async (req, res) => {
         id: req.user._id,
         name: req.user.name,
         email: req.user.email,
+        role: req.user.role
       },
     });
   } catch (error) {
